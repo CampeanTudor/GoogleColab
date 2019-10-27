d269e8ee145f3b034c5afca47d69c52134b77c4e
CampeanTudor <campeantudor97@gmail.com>
2019-10-27 19:28:56 +0200

    pip install and imports ar not in function

--- renderGymToColab/GymToColabAdaptor.py
+++ renderGymToColab/GymToColabAdaptor.py
@@ -10,37 +10,37 @@ Install packages
 """
 
 # Commented out IPython magic to ensure Python compatibility.
-def initialize_adaptor():
-  #remove " > /dev/null 2>&1" to see what is going on under the hood
-  !pip install gym pyvirtualdisplay > /dev/null 2>&1
-  !apt-get install -y xvfb python-opengl ffmpeg > /dev/null 2>&1
 
-  !apt-get update > /dev/null 2>&1
-  !apt-get install cmake > /dev/null 2>&1
-  !pip install --upgrade setuptools 2>&1
-  !pip install ez_setup > /dev/null 2>&1
-  !pip install gym[atari] > /dev/null 2>&1
-  !pip install pyglet==1.3.2
+#remove " > /dev/null 2>&1" to see what is going on under the hood
+!pip install gym pyvirtualdisplay > /dev/null 2>&1
+!apt-get install -y xvfb python-opengl ffmpeg > /dev/null 2>&1
 
-  import gym
-  from gym import logger as gymlogger
-  from gym.wrappers import Monitor
-  gymlogger.set_level(40) #error only
-  import tensorflow as tf
-  import numpy as np
-  import random
-  import matplotlib
-  import matplotlib.pyplot as plt
+!apt-get update > /dev/null 2>&1
+!apt-get install cmake > /dev/null 2>&1
+!pip install --upgrade setuptools 2>&1
+!pip install ez_setup > /dev/null 2>&1
+!pip install gym[atari] > /dev/null 2>&1
+!pip install pyglet==1.3.2
+
+import gym
+from gym import logger as gymlogger
+from gym.wrappers import Monitor
+gymlogger.set_level(40) #error only
+import tensorflow as tf
+import numpy as np
+import random
+import matplotlib
+import matplotlib.pyplot as plt
 #   %matplotlib inline
-  import math
-  import glob
-  import io
-  import base64
-  from IPython.display import HTML
-  from IPython import display as ipythondisplay
-  from pyvirtualdisplay import Display
-  display = Display(visible=0, size=(1400, 900))
-  display.start()
+import math
+import glob
+import io
+import base64
+from IPython.display import HTML
+from IPython import display as ipythondisplay
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(1400, 900))
+display.start()
 
 """Import libraries
 
